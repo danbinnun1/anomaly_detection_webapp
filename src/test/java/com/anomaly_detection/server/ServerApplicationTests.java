bf1b993fa97f8dc1c2a9a3c8493ee6673007afe1
danbi <danbinnun1@gmail.com>
2021-04-26 12:52:53 +0300

    added spring boot template

---
+++ src/test/java/com/anomaly_detection/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anomaly_detection.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
