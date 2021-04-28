3c29bc8a8c7f0b78d3df8a20e00cf9b7bb0085c2
danbi <danbinnun1@gmail.com>
2021-04-28 19:59:16 +0300

    fixed bug

--- src/main/java/com/anomaly_detection/server/service/ModelService.java
+++ src/main/java/com/anomaly_detection/server/service/ModelService.java
@@ -3,17 +3,20 @@ package com.anomaly_detection.server.service;
 import com.anomaly_detection.server.model.Model;
 import com.anomaly_detection.server.repository.ModelRepository;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class ModelService {
     private final ModelRepository modelRepository;
     private ExecutorService executorService= Executors.newFixedThreadPool(20);
+
     public Model getById(Integer integer){
         //implement: if model doesn't exist throw exception, catch in controller and return 404
         return modelRepository.findById(integer).get();
