0b6d13a326f56fac413276a9c04e3a475de879f8
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2021-05-07 15:37:07 +0300

    Making Model accessors chainable

--- src/main/java/com/anomaly_detection/server/model/Model.java
+++ src/main/java/com/anomaly_detection/server/model/Model.java
@@ -1,19 +1,16 @@
 package com.anomaly_detection.server.model;
 
 import com.anomaly_detection.server.service.algorithms.TimeSeriesAnomalyDetector;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 @Data
+@Accessors(chain = true)
 @Document
 public class Model {
     @Id
