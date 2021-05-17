5f20d0d7e39d4866f40e566da610c1888d7ac2d7
danbinnun1 <danbinnun1@gmail.com>
2021-05-17 19:34:06 +0300

    added css

--- frontend/src/ModelsList.js
+++ frontend/src/ModelsList.js
@@ -3,12 +3,13 @@ import Model from './Model'
 
 export default function Models(props){
     return (
-        <ul class="list-group">
+        <div style={{position:'fixed',width:320,overflowY:'scroll',top:0,bottom:0}}>
           {props.models.map(item=>(
-              <li className="list-group-item list-group-item-primary">
+              <li style={{listStyleType:'none'}}>
                   <Model status={item.status} date={item.uploadTime}></Model>
+                  <br></br>
               </li>
           ))}
-        </ul>
+        </div>
     )
 }
