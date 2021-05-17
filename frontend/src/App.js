207bbfebd6aec0e7df815e7965fb4201b894d536
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2021-05-17 20:41:09 +0300

    Updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react'
 import Models from './Models'
-import FileUploadBox from './FileUploadBox'
+import DropZone from './DropZone'
 import convertCSVToJSON from './utils'
 
 class App extends React.Component {
@@ -16,16 +16,13 @@ class App extends React.Component {
         <div style={{ position: 'fixed', width: 320, overflowY: 'scroll', top: 10, bottom: '20%', left: '83%' }}>
           <Models />
         </div>
-        <div>
-          <FileUploadBox onUpload={(file) => {
-            const json = convertCSVToJSON(file);
-            fetch("/api/model/hybrid", {
-              method: 'POST',
-              body: json
-            });
-          }} />
-        </div>
-
+        <DropZone onUpload={async (file) => {
+          const json = await new Promise(resolve => resolve(convertCSVToJSON(file)));
+          await fetch("/api/model/hybrid", {
+            method: 'POST',
+            body: json
+          });
+        }}/>
       </div>
     );
   }
