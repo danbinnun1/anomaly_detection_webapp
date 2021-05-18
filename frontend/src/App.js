ec85cbc7cfb4ff99e1f4366d45cc51024aa0844f
danbinnun1 <danbinnun1@gmail.com>
2021-05-18 13:58:41 +0300

    added css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Models from './Models'
 import FileUploadBox from './FileUploadBox'
 import convertCSVToJSON from './utils'
-
+import TrainDataUpload from './TrainDataUpload'
 class App extends React.Component {
 
   constructor(props) {
@@ -13,16 +13,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div style={{ position: 'fixed', width: 320, overflowY: 'scroll', top: 10, bottom: '20%', left: '83%' }}>
+        <div style={{ position: 'fixed', width: '20%', overflowY: 'scroll', top: 10, bottom: '20%', left: '83%' }}>
           <Models />
         </div>
-        <FileUploadBox onUpload={async (file) => {
-          const json = await new Promise(resolve => resolve(convertCSVToJSON(file)));
-          await fetch("/api/model/hybrid", {
-            method: 'POST',
-            body: json
-          });
-        }}/>
+        <div style={{ position: 'fixed', bottom: '10%', right: '3%' }} >
+          <FileUploadBox onUpload={async (file) => {
+            const json = await new Promise(resolve => resolve(convertCSVToJSON(file)));
+            await fetch("/api/model/hybrid", {
+              method: 'POST',
+              body: json
+            });
+          }} />
+        </div>
       </div>
     );
   }
