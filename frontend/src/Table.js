19de2c5dbc736e1bd7088f1be151c84441b0d2d3
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2021-05-30 22:49:10 +0300

    Making things work

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -1,96 +1,108 @@
 import React, { useState } from 'react'
 import { convertJSONToLines } from './utils';
 
-export default function Table(props) {console.log(result);
-    var [result, setResult] = useState();
+export default class Table extends React.Component {
     
-    const getKeys = (data) => {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            result: undefined
+        }
+    }
+    
+    getKeys(data) {
         if (data === undefined || data.length === 0) {
             return [];
         }
         return data[0];
     }
 
-    const table = {
-        borderRadius: "30px",
-        fontSize: "20px",
-        fontWeight: "normal",
-        border: "none",
-        borderCollapse: "collapse",
-        width: "100%",
-        maxWidth: "100%",
-        whiteSpace: "nowrap",
-        borderSpacing: "10px",
-        textAlign: "center",
-        columnWidth: "100px"
-    };
-
-    const thead = {
-        color: "#ffffff",
-        background: "#000000",
-    };
-    const tbody = {
-        color: "#000000",
-        background: "ffffff",
-    };
-
-    const getHeader = (data) => {
-        var keys = getKeys(data);
+    getHeader(data) {
+        var keys = this.getKeys(data);
         return keys.map((key, index) => {
             return <th key={key}>{key.toUpperCase()}</th>
         })
     }
 
-    const getColor=(row,col,data)=>{
-        if (props.anomalies===undefined){
+    getColor(row, col, data) {
+        if (this.props.anomalies === undefined){
             return 'white';
         }
-        let key=getKeys(data)[col];
-        for (let span of props.anomalies[key]){
-            if (row>=span.start&&row<=span.end){
+        let key = this.getKeys(data)[col];
+        for (let span of this.props.anomalies[key]){
+            if (row >= span.start && row <= span.end){
                 return 'red';
             }
         }
         return 'green';
     }
 
-    const RenderRow = (row, rowIndex,data) => {
+    renderRow(row, rowIndex,data) {
         return row.map((key, index) => {
-            return <td style={{backgroundColor: getColor(rowIndex,index,data)}} >{key}</td>
+            return <td style={{backgroundColor: this.getColor(rowIndex,index,data)}} >{key}</td>
         })
     }
 
-    const getRowsData = (data) => {
+    getRowsData(data) {
         return data.slice(1).map((row, index) => {
             return <tr key={index}>
-                {RenderRow(row,index,data)}
+                {this.renderRow(row, index, data)}
             </tr>
         })
     }
 
-    if (props.data === undefined) {
-        return (null);
+    render() {
+        const table = {
+            borderRadius: "30px",
+            fontSize: "20px",
+            fontWeight: "normal",
+            border: "none",
+            borderCollapse: "collapse",
+            width: "100%",
+            maxWidth: "100%",
+            whiteSpace: "nowrap",
+            borderSpacing: "10px",
+            textAlign: "center",
+            columnWidth: "100px"
+        };
+    
+        const thead = {
+            color: "#ffffff",
+            background: "#000000",
+        };
+        const tbody = {
+            color: "#000000",
+            background: "ffffff",
+        };
+
+        if (this.props.data === undefined) {
+            return (null);
+        }
+    
+        if (this.state.result === undefined || this.state.result.length === 0) {
+            setTimeout(() => {
+                const data = convertJSONToLines(this.props.data);
+                this.setState({
+                    result: <div>
+                                <table style={table} cellpadding="10">
+                                    <thead style = {thead}>
+                                        <tr>{this.getHeader(data)}</tr>
+                                    </thead>
+                                    <tbody style = {tbody}>
+                                        {this.getRowsData(data)}
+                                    </tbody>
+                                </table>
+                            </div>
+                });
+            });
+    
+            return <h1>Loading...</h1>
+        }
+    
+        const temp = this.state.result;
+        this.state.result = undefined;
+    
+        return (temp);
     }
-
-    if (result === undefined || result.length === 0) {
-        setTimeout(() => {
-            const data = convertJSONToLines(props.data);
-            setResult(<div>
-                <table style = {table} cellpadding="10">
-                    <thead style = {thead}>
-                        <tr>{getHeader(data)}</tr>
-                    </thead>
-                    <tbody style = {tbody}>
-                        {getRowsData(data)}
-                    </tbody>
-                </table>
-            </div>);
-        }, 1);
-
-        return <h1>Loading...</h1>
-    }
-
-    return (
-        result
-    );
 }
