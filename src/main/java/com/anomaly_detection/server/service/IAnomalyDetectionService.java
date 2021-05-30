cc1c1885afc474a5f43ad3ee378e08c564c867ef
danbinnun1 <danbinnun1@gmail.com>
2021-05-30 20:36:46 +0300

    renamed server files

---
+++ src/main/java/com/anomaly_detection/server/service/IAnomalyDetectionService.java
@@ -0,0 +1,13 @@
+package com.anomaly_detection.server.service;
+
+import com.anomaly_detection.server.dto.Anomaly;
+import com.anomaly_detection.server.exceptions.InvalidDataException;
+import com.anomaly_detection.server.exceptions.ModelNotFoundException;
+import com.anomaly_detection.server.exceptions.TrainingNotFinishedException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IAnomalyDetectionService {
+    Anomaly detect(Map<String, List<Float>> data, String modelId) throws InvalidDataException, TrainingNotFinishedException, ModelNotFoundException;
+}
---
+++ src/main/java/com/anomaly_detection/server/service/IModelTrainingService.java
@@ -0,0 +1,11 @@
+package com.anomaly_detection.server.service;
+
+import com.anomaly_detection.server.dto.ModelDto;
+import com.anomaly_detection.server.exceptions.TypeNotSupportedException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IModelTrainingService {
+    ModelDto trainModel(Map<String, List<Float>> data, String type) throws TypeNotSupportedException;
+}
