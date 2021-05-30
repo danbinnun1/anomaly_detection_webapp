9738ebf2b418173d749772841797c364bbf2fed9
danbinnun1 <danbinnun1@gmail.com>
2021-05-30 19:09:31 +0300

    fixed bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </HashRouter>,
+  document.getElementById("root")
 );
+
+serviceWorker.unregister();
